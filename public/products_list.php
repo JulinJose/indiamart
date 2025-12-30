0d4e084f15f68e59640f1b35281e78f33ade6f60
Julin <julinjose20@gmail.com>
2025-12-30 11:39:18 +0530

    add code

--- public/products_list.php
+++ public/products_list.php
@@ -2,18 +2,25 @@
 header("Content-Type: application/json");
 include("../admin/connection.php");
 
-// Fetch all products
-$q = mysqli_query($db,"SELECT id, name, img, old_price, new_price, stock FROM products ORDER BY id DESC");
+// Fetch only products that are in stock
+$q = mysqli_query(
+    $db,
+    "SELECT id, name, img, old_price, new_price, stock 
+     FROM products 
+     WHERE stock > 0
+     ORDER BY id DESC"
+);
 
 $products = [];
-while($r = mysqli_fetch_assoc($q)) {
+
+while ($r = mysqli_fetch_assoc($q)) {
     $products[] = [
-        "id" => $r['id'],
+        "id" => (int)$r['id'],
         "name" => $r['name'],
-        "image" => "https://design-pods.com/indiamart/admin/assets/images/products/".$r['img'],
-        "old_price" => $r['old_price'],
-        "new_price" => $r['new_price'],
-        "stock" => $r['stock']
+        "image" => "https://design-pods.com/indiamart/admin/assets/images/products/" . $r['img'],
+        "old_price" => (float)$r['old_price'],
+        "new_price" => (float)$r['new_price'],
+        "stock" => (int)$r['stock']
     ];
 }
 
@@ -21,4 +28,5 @@ echo json_encode([
     "status" => "success",
     "products" => $products
 ]);
+exit;
 ?>
